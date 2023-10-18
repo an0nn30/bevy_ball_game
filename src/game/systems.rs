51b523fc6a0ea8f71d8865f76eb0f3b93eb3128e
an0nn30 <70341401+an0nn30@users.noreply.github.com>
2023-10-18 22:16:31 +0200

    added ui for main menu

--- src/game/systems.rs
+++ src/game/systems.rs
@@ -2,17 +2,17 @@ use crate::game::SimulationState;
 use bevy::prelude::*;
 
 pub fn toggle_simulation(
-    mut commands: Commands,
     keyboard_input: Res<Input<KeyCode>>,
     simulation_state: Res<State<SimulationState>>,
+    mut next_app_state: ResMut<NextState<SimulationState>>,
 ) {
     if keyboard_input.just_pressed(KeyCode::P) {
         if simulation_state.0 == SimulationState::Running {
-            commands.insert_resource(NextState(Some(SimulationState::Paused)));
+            next_app_state.set(SimulationState::Paused);
             println!("Simulation Paused");
         }
         if simulation_state.0 == SimulationState::Paused {
-            commands.insert_resource(NextState(Some(SimulationState::Running)));
+            next_app_state.set(SimulationState::Running);
             println!("Simulation Running");
         }
     }
